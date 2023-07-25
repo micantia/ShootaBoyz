9b282360b90676120cd5ffec0fe5f85e0f160d7f
aliaksandr.nestsiarenka <aliaksandr.nestsiarenka@outlook.com>
2023-07-25 20:58:30 +0300

    Initial commit

---
+++ Source/Shoota/ShootaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShootaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTA_API AShootaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
